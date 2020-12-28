e92046d5948cb094b5de7277b999ce33907fb03f
Satyam Gupta <satyamg650@gmail.com>
2020-12-28 18:19:47 +0530

    Initial commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // }old code without using rectangle module(require function)
 var rect = require('./rectangle');
 
+
 function solveRect(l,b){
     console.log("Solving for rectange with l = "+l+" and breadth b = "+b);
 
